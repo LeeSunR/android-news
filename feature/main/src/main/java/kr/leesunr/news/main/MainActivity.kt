47c092ecb39f8641db208df7baea74620644b6ee
이선룡 <sd844@naver.com>
2024-10-18 12:08:41 +0900

    add: 컴프즈 라이브러리 추가

--- feature/main/src/main/java/kr/leesunr/news/main/MainActivity.kt
+++ feature/main/src/main/java/kr/leesunr/news/main/MainActivity.kt
@@ -1,10 +1,15 @@
 package kr.leesunr.news.main
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Text(text = "Hello, World!")
+        }
     }
 }
