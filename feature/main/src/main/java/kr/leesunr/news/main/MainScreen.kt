ae2b5963bf9152754a71402c5dcfb1e7996dfad8
이선룡 <sd844@naver.com>
2024-10-20 16:57:50 +0900

    fix: 사용하지 않는 기능 UI 제거

--- feature/main/src/main/java/kr/leesunr/news/main/MainScreen.kt
+++ feature/main/src/main/java/kr/leesunr/news/main/MainScreen.kt
@@ -4,21 +4,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import kr.leesunr.news.headline.HeadlineScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,15 +29,6 @@ fun MainScreen(
                     modifier = Modifier.fillMaxWidth(),
                     text = stringResource(R.string.app_bar_title),
                     textAlign = TextAlign.Center
-                )
-            },
-            actions = {
-                Icon(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .size(24.dp),
-                    painter = painterResource(id = R.drawable.settings_24px),
-                    contentDescription = ""
                 )
             },
             colors = TopAppBarDefaults.topAppBarColors(
